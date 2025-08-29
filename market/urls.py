30c745ec8c5fc2739a72c7d729aa3ad52d18835d
Ismail gihozo <gihozoismail@gmial.com>
2025-08-29 12:51:36 +0200

    owner product all neede functionality

---
+++ market/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path,include
+from rest_framework import routers
+
+from .views import (ProductView,
+                    OwnerView,
+                    OwnerProductsView,
+                    ProductListView,
+                    ProductDetailView,
+                    OwnerDeleteProduct,
+                    OwnerEditProductView)
+
+
+
+router=routers.DefaultRouter()
+router.register("products",ProductView,basename="products")
+router.register("owners",OwnerView,basename="owners")
+
+
+
+
+
+urlpatterns=[
+    path("",include(router.urls)),
+    path("all-products/",ProductListView.as_view()),
+    path("product/<int:product_id>/",ProductDetailView.as_view()),
+    path("owner/<int:owner_id>/products/",OwnerProductsView.as_view()),
+path('owner-product/<int:product_id>/edit/', OwnerEditProductView.as_view(), name='owner-edit-product'),
+path("owner-product/<int:product_id>/delete/", OwnerDeleteProduct.as_view(), name='owner-delete-product')
+
+
+]
